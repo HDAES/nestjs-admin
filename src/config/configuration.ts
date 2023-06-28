912113f57d61f6d34958a61ccf3dd1d2ff4711d7
xiaolei <lei.xiao@houhoutech.com>
2023-06-28 17:13:16 +0800

    feat: 增加 配置信息

---
+++ src/config/configuration.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
+import { merge } from 'lodash';
+
+const YAML_CONFIG_FILENAME = 'config.yaml';
+const YAML_CONFIG_ENV = `config.${process.env.NODE_ENV || 'development'}.yaml`;
+
+const filePath = yaml.load(
+  readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+) as Record<string, any>;
+
+const envPath = yaml.load(
+  readFileSync(join(__dirname, YAML_CONFIG_ENV), 'utf8'),
+) as Record<string, any>;
+
+console.log(merge(filePath, envPath));
+export default () => {
+  return merge(filePath, envPath);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import Swagger from './utils/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   new Swagger(app);
-  await app.listen(3000);
+  await app.listen(configService.get('http')['port'] || '3000');
 }
 bootstrap();
